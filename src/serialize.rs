4cdba699c741858de51a5cafa1b6196674083dd0
Ygg01 <y.laughing.man.y@gmail.com>
2022-01-15 04:15:43 +0100

    Implement serialization for actions only.

---
+++ src/serialize.rs
@@ -0,0 +1,78 @@
+use std::fmt::Formatter;
+use std::hash::Hash;
+use std::marker::PhantomData;
+
+use serde::de::{MapAccess, Visitor};
+use serde::ser::SerializeMap;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+use crate::InputMap;
+
+#[cfg(feature = "serialize")]
+impl<T: Serialize> Serialize for InputMap<T> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut map = serializer.serialize_map(Some(self.actions.len()))?;
+        for (k, v) in &self.actions {
+            map.serialize_entry(k, v)?;
+        }
+        map.end()
+    }
+}
+
+#[cfg(feature = "serialize")]
+impl<'de, T> Deserialize<'de> for InputMap<T>
+where
+    T: Eq + Hash + Deserialize<'de>,
+{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let visitor = InputMapVisitor::<T>::new();
+
+        deserializer.deserialize_map(visitor)
+    }
+}
+
+#[cfg(feature = "serialize")]
+struct InputMapVisitor<K> {
+    marker: PhantomData<fn() -> InputMap<K>>,
+}
+
+impl<T> InputMapVisitor<T> {
+    pub(crate) fn new() -> Self {
+        InputMapVisitor {
+            marker: PhantomData,
+        }
+    }
+}
+
+#[cfg(feature = "serialize")]
+impl<'de, T> Visitor<'de> for InputMapVisitor<T>
+where
+    T: Deserialize<'de> + Eq + Hash,
+{
+    type Value = InputMap<T>;
+
+    fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+        formatter.write_str("Expected a valid Input mapping")
+    }
+
+    fn visit_map<M>(self, mut access: M) -> Result<Self::Value, M::Error>
+    where
+        M: MapAccess<'de>,
+    {
+        let mut map = InputMap::<T>::default();
+
+        // While there are entries remaining in the input, add them
+        // into our map.
+        while let Some((key, value)) = access.next_entry()? {
+            map.actions.insert(key, value);
+        }
+
+        Ok(map)
+    }
+}
